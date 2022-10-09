614fc78cb6361a9423f1e84b9b1aef0c1cf69646
Alaa A.E <80849327+alaa1945@users.noreply.github.com>
2022-10-09 21:25:37 +0300

    first

--- javascript/app.js
+++ javascript/app.js
@@ -12,3 +12,22 @@ hamburgerBtn.addEventListener("click", function () {
     });
   });
 });
+
+// scroll animation
+const scrollTracker = document.querySelector(".scroll-tracker");
+
+const scrollTrackingTimeline = new ScrollTimeline({
+  source: document.scrollingElement,
+  orientation: "block",
+  scrollOffsets: [CSS.percent(0), CSS.percent(100)],
+});
+
+scrollTracker.animate(
+  {
+transform:['scaleX(0)','scaleX(1)']
+  },
+  {
+    duration:1,
+    timeline:scrollTrackingTimeline,
+  }
+  );
